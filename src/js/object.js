15043a1c9cfa37e7b7d0283c58d5c392993da241
Chris Richardson <cjr@hotmail.co.uk>
2016-09-21 11:52:19 +0100

    delete_key & delete - new fns to remove object keys

--- src/js/object.js
+++ src/js/object.js
@@ -1,3 +1,25 @@
-function has_key(o, key) {
-  return Object.keys(o).indexOf(key) !== -1;
+function has_key(o, k) {
+  return Object.keys(o).indexOf(k) !== -1;
+}
+
+function delete_key(o, k, d) {
+  if (not_defined(o)) {
+    return d;
+  } else if (not_defined(k) || not_defined(o[k])) {
+    return d;
+  } else {
+    var v = o[k];
+    delete o[k];
+    return v;
+  }
+}
+
+function qp_delete(o, k) {
+  if (not_defined(o)) {
+    o = { };
+  } else if (not_defined(k) || not_defined(o[k])) {
+  } else {
+    delete o[k];
+  }
+  return o;
 }
