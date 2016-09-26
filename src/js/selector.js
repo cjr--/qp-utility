a396ace436c9ff0e87d034b66ff3b9f8eb6680c1
Chris Richardson <cjr@hotmail.co.uk>
2016-09-26 19:44:21 +0100

    selector - select_children

--- src/js/selector.js
+++ src/js/selector.js
@@ -1,15 +1,20 @@
 function select_all() {
   var one_arg = arguments.length === 1;
+  if (!one_arg && !is_element(arguments[1])) return [];
   var element = one_arg ? document : arguments[0];
   var selector = one_arg ? arguments[0] : arguments[1];
-  var elements;
-  var class_name = selector.match(class_re);
-  if (class_name) {
-    elements = element.getElementsByClassName(class_name[1]);
-  } else {
-    elements = element.querySelectorAll(selector);
-  }
-  return slice.call(elements);
+  return slice.call(element.querySelectorAll(selector));
+}
+
+function select_children(element, selector) {
+  if (!is_element(element)) return [];
+  var id = element.id;
+  var guid = element.id = (id || 'qp' + qp_id());
+  var scope = '#' + guid + ' > ';
+  selector = scope + (selector + '').replace(',', ',' + scope, 'g');
+  var result = slice.call(element.parentNode.querySelectorAll(selector));
+  if (!id) element.removeAttribute('id');
+  return result;
 }
 
 function matches(el, selector) {
