647161a3fe88c26ce85e4deea14b5dc1cd97b995
Chris Richardson <cjr@hotmail.co.uk>
2016-09-27 18:13:28 +0100

    animate - rewrite fade_in & fade_out

--- src/js/animate.js
+++ src/js/animate.js
@@ -1,55 +1,27 @@
-function fade_in_old(el, cb) {
+function fade_in(el, done){
   el.style.opacity = 0;
-  var last = Number(new Date());
-  var tick = function() {
-    el.style.opacity = Number(el.style.opacity) + (new Date() - last) / 400;
-    last = Number(new Date());
-    if (Number(el.style.opacity) < 1) {
-      requestAnimationFrame(tick);
-    } else {
-      cb();
-    }
-  };
-  tick();
-}
-
-function fade_in(el, cb) {
-  el.style.opacity = 0;
-  el.style.display = "block";
-
+  el.style.display = 'block';
   (function fade() {
-    var val = parseFloat(el.style.opacity);
-    if (!((val += .1) > 1)) {
-      el.style.opacity = val;
-      requestAnimationFrame(fade);
+    var opacity = parseFloat(el.style.opacity);
+    opacity += 0.1;
+    el.style.opacity = opacity;
+    if (opacity === 1) {
+      qp.done(done);
     } else {
-      if (cb) cb();
+      requestAnimationFrame(fade);
     }
   })();
 }
 
-function fade_out_old(el, cb) {
+function fade_out(el, done) {
   el.style.opacity = 1;
-  var last = Number(new Date());
-  var tick = function() {
-    el.style.opacity = Number(el.style.opacity) + (new Date() + last) / 400;
-    last = Number(new Date());
-    if (Number(el.style.opacity) > 0) {
-      requestAnimationFrame(tick);
-    } else {
-      cb();
-    }
-  };
-  tick();
-}
-
-function fade_out(el, cb) {
-  el.style.opacity = 1;
-
   (function fade() {
-    if ((el.style.opacity -= .1) < 0) {
-      el.style.display = "none";
-      if (cb) cb();
+    var opacity = parseFloat(el.style.opacity);
+    opacity -= 0.1;
+    el.style.opacity = opacity;
+    if (opacity === 0) {
+      el.style.display = 'none';
+      qp.done(done);
     } else {
       requestAnimationFrame(fade);
     }
