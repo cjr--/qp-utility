6f2c3b37d25bd026446f1e24789246406d2ee508
Chris Richardson <cjr@hotmail.co.uk>
2016-09-30 11:40:22 +0100

    animate - refactor

--- src/js/animate.js
+++ src/js/animate.js
@@ -1,29 +1,43 @@
-function fade_in(el, done){
-  el.style.opacity = 0;
-  el.style.display = 'block';
-  (function fade() {
-    var opacity = parseFloat(el.style.opacity);
-    opacity += 0.1;
-    el.style.opacity = opacity;
-    if (opacity === 1) {
-      qp.done(done);
-    } else {
-      requestAnimationFrame(fade);
-    }
-  })();
-}
+function animate(o) {
+  var effect = o.effect || 'none';
+  var done = o.done || noop;
+  var el = o.el;
+  var interval = o.interval;
 
-function fade_out(el, done) {
-  el.style.opacity = 1;
-  (function fade() {
+  if (effect === 'none' || not_element(el)) {
+    invoke_next(done);
+  } else if (effect === 'fade_in') {
+    el.style.opacity = 0;
+    el.style.display = 'block';
+    interval = interval || 0.25;
+    fade_in();
+  } else if (effect === 'fade_out') {
+    el.style.opacity = 1;
+    interval = interval || 0.25;
+    fade_out();
+  }
+
+  function fade_in() {
     var opacity = parseFloat(el.style.opacity);
-    opacity -= 0.1;
+    opacity += interval;
     el.style.opacity = opacity;
-    if (opacity === 0) {
-      el.style.display = 'none';
+    if (opacity >= 1) {
       qp.done(done);
     } else {
-      requestAnimationFrame(fade);
+      requestAnimationFrame(fade_in);
     }
-  })();
+  }
+
+  function fade_out() {
+    var opacity = parseFloat(el.style.opacity);
+    opacity -= interval;
+    el.style.opacity = opacity;
+    if (opacity <= 0) {
+      el.style.display = 'none';
+      done();
+    } else {
+      requestAnimationFrame(fade_out);
+    }
+  }
+
 }
