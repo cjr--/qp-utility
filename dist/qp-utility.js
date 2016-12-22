e176c05741b0d51d6cb38d504e9486aa12571e01
Chris Richardson <cjr@hotmail.co.uk>
2016-12-22 14:06:15 +0000

    build

--- dist/qp-utility.js
+++ dist/qp-utility.js
@@ -1748,7 +1748,7 @@
         }, 0);
       } else {
         return o.reduce(function(sum, item) {
-          return sum + Number(item); 
+          return sum + Number(item);
         }, 0);
       }
     }
@@ -1768,6 +1768,34 @@
   function avg(o, k) { return sum(o, k) / (o.length || 1); }
   function max(o, k) { return min_max(o, k, 'max'); }
   function min(o, k) { return min_max(o, k, 'min'); }
+  
+  function round(n, decimals) {
+    return Number(Math.round(n + 'e' + decimals) + 'e-' + decimals);
+  }
+  
+  function truncate(n, decimals) {
+    var pow = Math.pow(10, decimals);
+    return ~~(n * pow) / pow;
+  }
+  
+  function clamp(n, min, max) {
+    return Math.min(Math.max(n, min), max);
+  }
+  
+  function round_currency(v, decimals) {
+    var d = decimals || 0;
+    var m = Math.pow(10, d);
+    var n = +(d ? v * m : v).toFixed(8);
+    var i = Math.floor(n);
+    var f = n - i;
+    var e = 1e-8;
+    var r = (f > 0.5 - e && f < 0.5 + e) ? ((i % 2 == 0) ? i : i + 1) : Math.round(n);
+    return d ? r / m : r;
+  }
+  
+  function currency(n, currency, locale) {
+    return Number(n).toLocaleString(locale || 'en-GB', { style: 'currency', currency: currency || 'GBP' });
+  }
   
   function build_match(s) {
     var escape = '/$^+.()=!|[]{},';
@@ -2266,6 +2294,11 @@
     min: min,
     max: max,
     avg: avg,
+    round: round,
+    truncate: truncate,
+    clamp: clamp,
+    round_currency: round_currency,
+    currency: currency,
     map: map,
     reduce: reduce,
     arg: arg,
