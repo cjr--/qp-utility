349387a3e7cc4d7d2b1d8a6527964a0d999b853a
Chris Richardson <cjr@hotmail.co.uk>
2017-01-28 14:07:59 +0000

    select - check array is passed

--- src/js/select.js
+++ src/js/select.js
@@ -1,10 +1,12 @@
 function select(items, fn, scope) {
   var out = [];
-  var args = rest(arguments, 2);
-  for (var i = 0, l = items.length; i < l; i++) {
-    args[0] = items[i];
-    var result = fn.apply(scope, args);
-    if (result !== undefined) out.push(result);
+  if (is_array(items)) {
+    var args = rest(arguments, 2);
+    for (var i = 0, l = items.length; i < l; i++) {
+      args[0] = items[i];
+      var result = fn.apply(scope, args);
+      if (result !== undefined) out.push(result);
+    }
   }
   return out;
 }
