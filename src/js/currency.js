df27478b10f0901002be59fa78ac65cf4d27da42
Chris Richardson <cjr@hotmail.co.uk>
2017-02-20 17:41:32 +0000

    currency - clean input string before formatting

--- src/js/currency.js
+++ src/js/currency.js
@@ -1,3 +1,4 @@
 function currency(n, currency, locale) {
-  return Number(n).toLocaleString(locale || 'en-GB', { style: 'currency', currency: currency || 'GBP' });
+  var value = Number(String(n).replace(/[^0-9.]/g, ''));
+  return value.toLocaleString(locale || 'en-GB', { style: 'currency', currency: currency || 'GBP' });
 }
