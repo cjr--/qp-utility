b4dfa305cad04608fc7ca07c90b03a2c184e2f0b
Chris Richardson <cjr@hotmail.co.uk>
2017-02-21 13:22:23 +0000

    currency - break out fn for cleaning input value

--- src/js/currency.js
+++ src/js/currency.js
@@ -1,4 +1,8 @@
-function currency(n, currency, locale) {
-  var value = Number(String(n).replace(/[^0-9.]/g, ''));
+function currency(o, currency, locale) {
+  var value = currency_value(o);
   return value.toLocaleString(locale || 'en-GB', { style: 'currency', currency: currency || 'GBP' });
+}
+
+function currency_value(o) {
+  return Number(String(o).replace(/[^0-9.]/g, ''));
 }
