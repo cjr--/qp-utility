b9605e518b40def8e2bdb7f2b5dba72596a4b1f7
Chris Richardson <cjr@hotmail.co.uk>
2017-04-12 16:25:36 +0100

    build

--- dist/qp-utility.js
+++ dist/qp-utility.js
@@ -1806,8 +1806,23 @@
     return Math.min(Math.max(n, min), max);
   }
   
-  function round_currency(v, decimals) {
-    var d = decimals || 0;
+  function currency(code) {
+    var lookup = {
+      USD: { code: 'USD', name: 'Dollar', symbol: '$', decimals: 2, pow: 100 },
+      EUR: { code: 'EUR', name: 'Euro', symbol: '€', decimals: 2, pow: 100 },
+      GBP: { code: 'GBP', name: 'Pound', symbol: '£', decimals: 2, pow: 100 },
+      JPY: { code: 'JPY', name: 'Yen', symbol: '¥', decimals: 0, pow: 0 },
+      AUD: { code: 'AUD', name: 'Dollar', symbol: '$', decimals: 2, pow: 100 },
+      CAD: { code: 'CAD', name: 'Dollar', symbol: '$', decimals: 2, pow: 100 },
+      CHF: { code: 'CHF', name: 'Franc', symbol: 'Fr', decimals: 2, pow: 100 },
+      CNY: { code: 'CNY', name: 'Yuan', symbol: '¥', decimals: 2, pow: 100 },
+      RUB: { code: 'RUB', name: 'Ruble', symbol: '₽', decimals: 2, pow: 100 }
+    };
+    return lookup[code] ? lookup[code] : null;
+  }
+  
+  function bankers_round(v, d) {
+    d = d || 0;
     var m = Math.pow(10, d);
     var n = +(d ? v * m : v).toFixed(8);
     var i = Math.floor(n);
@@ -1817,14 +1832,94 @@
     return d ? r / m : r;
   }
   
-  function currency(o, currency, locale) {
-    var value = currency_value(o);
-    return value.toLocaleString(locale || 'en-GB', { style: 'currency', currency: currency || 'GBP' });
+  function calculate_precision(number) {
+    var match = String(number).match(/(?:\.(\d+))?(?:[eE]([+-]?\d+))?$/);
+    if (!match) { return 0; }
+    return Math.max(0, (match[1] ? match[1].length : 0) - (match[2] ? +match[2] : 0));
   }
   
-  function currency_value(o) {
-    return Number(String(o).replace(/[^0-9.]/g, ''));
+  
+  function decimal(number, precision) {
+    if (number instanceof Decimal) {
+      return number.clone();
+    } else {
+      return new Decimal(number, precision);
+    }
   }
+  
+  function Decimal(number, precision) {
+    this.input = number;
+    this.precision = (typeof precision === 'number' ? precision : calculate_precision(number));
+    this.pow = Math.pow(10, this.precision);
+    this.int_value = bankers_round(number * this.pow);
+    this.value = this.int_value / this.pow;
+  }
+  
+  Decimal.prototype.toString = function() { return this.value; };
+  
+  Decimal.prototype.clone = function() {
+    return new Decimal(this.input, this.precision);
+  };
+  
+  
+  function money(currency_code, value, locale) {
+    if (currency_code instanceof Money) {
+      return currency_code.clone();
+    } else {
+      return new Money(currency_code, value, locale);
+    }
+  }
+  
+  function Money(currency_code, value, locale) {
+    this.input = value;
+    this.locale = locale || navigator.language;
+    this.currency = currency(currency_code);
+    if (this.currency) {
+      if (typeof value === 'string') {
+        var sign = value.indexOf('-') !== -1 ? '-' : '';
+        value = value.replace(/[^0-9.]/g, '');
+        var parts = value.split('.');
+        value = sign + (parts[0] || '0') + (this.currency.decimals ? '.' + (parts[1] || '0') : '');
+        value = Number(value);
+        this.int_value = Math.round(value * this.currency.pow);
+      } else {
+        this.int_value = bankers_round(value, this.currency.decimals) * this.currency.pow;
+      }
+      this.precision = this.currency.decimals;
+      this.value = this.int_value / this.currency.pow;
+      this.display = this.value.toLocaleString(this.locale, { style: 'currency', currency: this.currency.code });
+    } else {
+      this.precision = 0;
+      this.int_value = this.value = 0;
+      this.display = '0';
+    }
+  }
+  
+  Money.prototype.toString = function() { return this.display; };
+  
+  Money.prototype.clone = function() {
+    return new Money(this.currency.code, this.input, this.locale);
+  };
+  
+  Money.prototype.add = function(value) {
+    var money = value instanceof Money ? value : new Money(this.currency.code, value, this.locale);
+    return new Money(this.currency.code, (this.int_value + money.int_value) / this.currency.pow, this.locale);
+  };
+  
+  Money.prototype.subtract = function(value) {
+    var money = value instanceof Money ? value : new Money(this.currency.code, value, this.locale);
+    return new Money(this.currency.code, (this.int_value - money.int_value) / this.currency.pow, this.locale);
+  };
+  
+  Money.prototype.divide = function(value) {
+    var decimal = value instanceof Decimal ? value : new Decimal(value);
+    return new Money(this.currency.code, bankers_round((this.value / decimal.value), this.precision), this.locale);
+  };
+  
+  Money.prototype.multiply = function(value) {
+    var decimal = value instanceof Decimal ? value : new Decimal(value);
+    return new Money(this.currency.code, bankers_round(this.value * decimal.value, (this.precision + decimal.precision)), this.locale);
+  };
   
   function build_match(s) {
     var escape = '/$^+.()=!|[]{},';
@@ -2431,9 +2526,11 @@
     round: round,
     truncate: truncate,
     clamp: clamp,
-    round_currency: round_currency,
     currency: currency,
-    currency_value: currency_value,
+    Money: Money,
+    money: money,
+    Decimal: Decimal,
+    decimal: decimal,
     map: map,
     reduce: reduce,
     arg: arg,
