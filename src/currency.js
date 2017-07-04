8d6d53d8486525cf39dfef4c619fa74a4614e189
Chris Richardson <cjr@hotmail.co.uk>
2017-07-04 14:09:38 +0100

    Currency - handle no currency code better

--- src/currency.js
+++ src/currency.js
@@ -95,7 +95,7 @@ function Money(currency_code, value, o) {
   } else if (is(currency_code, 'object')) {
     this.currency = currency_code;
   }
-  if (this.currency) {
+  if (this.currency && this.currency.code !== '') {
     if (typeof value === 'string') {
       var sign = value.indexOf('-') !== -1 ? '-' : '';
       value = value.replace(/[^0-9.]/g, '');
@@ -115,9 +115,10 @@ function Money(currency_code, value, o) {
       maximumFractionDigits: this.currency.decimals
     });
   } else {
-    this.precision = 0;
-    this.int_value = this.value = 0;
-    this.entry = this.display = '0';
+    this.currency = { code: '', decimals: 2, pow: 100 };
+    this.precision = 0.00;
+    this.int_value = this.value = 0.00;
+    this.entry = this.display = '0.00';
   }
 }
 
