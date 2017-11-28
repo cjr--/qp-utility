c409a0ad8a52cbec847a95fa32435e4388ddb468
Chris Richardson <cjr@hotmail.co.uk>
2017-11-28 19:39:37 +0000

    build

--- dist/index.js
+++ dist/index.js
@@ -1314,6 +1314,8 @@
       each(o, function(k) { if (source.hasOwnProperty(k)) target[k] = source[k]; });
     } else if (is(o, 'function')) {
       each_own(source, function(v, k) { if (o(v, k, i)) target[k] = v; });
+    } else if (is(o, 'string')) {
+      return pick(source, rest());
     }
     return target;
   }
