e559d88a61b388d909ff908b63cfbd9172c50f5e
Chris Richardson <cjr@hotmail.co.uk>
2017-11-28 19:39:28 +0000

    pick; handle params key list

--- src/pick.js
+++ src/pick.js
@@ -4,6 +4,8 @@ function pick(source, o) {
     each(o, function(k) { if (source.hasOwnProperty(k)) target[k] = source[k]; });
   } else if (is(o, 'function')) {
     each_own(source, function(v, k) { if (o(v, k, i)) target[k] = v; });
+  } else if (is(o, 'string')) {
+    return pick(source, rest());
   }
   return target;
 }
