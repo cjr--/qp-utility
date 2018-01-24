dc9d334f544ef89d1821d9d890e9dfec21bb6bdf
Chris Richardson <cjr@hotmail.co.uk>
2018-01-24 21:29:05 +0000

    pick; bug fixes

--- src/pick.js
+++ src/pick.js
@@ -1,11 +1,13 @@
 function pick(source, o) {
   var target = { };
-  if (is(o, 'array')) {
-    each(o, function(k) { if (source.hasOwnProperty(k)) target[k] = source[k]; });
-  } else if (is(o, 'function')) {
-    each_own(source, function(v, k) { if (o(v, k, i)) target[k] = v; });
-  } else if (is(o, 'string')) {
-    return pick(source, rest());
+  if (is(source, 'object')) {
+    if (is(o, 'array')) {
+      each(o, function(k) { if (source.hasOwnProperty(k)) target[k] = source[k]; });
+    } else if (is(o, 'function')) {
+      each_own(source, function(v, k) { if (o(v, k, i)) target[k] = v; });
+    } else if (is(o, 'string')) {
+      return pick(source, rest(arguments));
+    }
   }
   return target;
 }
