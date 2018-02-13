8226541aa9d956605da6b913e8f1867410264038
Chris Richardson <cjr@hotmail.co.uk>
2018-02-13 19:12:14 +0000

    client socket; reconnect

--- dist/qp-utility.js
+++ dist/qp-utility.js
@@ -2947,7 +2947,8 @@
       protocols: null,
       json: false,
   
-      retry_interval: 2000,
+      retry_id: 0,
+      retry_interval: 5000,
       retry_count: 0,
       retry_max: Infinity,
   
@@ -2955,6 +2956,8 @@
       on_close: false,
       on_open: false,
       on_data: false,
+      on_reconnect: false,
+      on_reconnect_failed: false,
   
       init: function(options) {
         if (this.protocols) this.json = qp.contains(this.protocols, 'json-message');
@@ -2966,27 +2969,31 @@
       },
   
       connect: function() {
+        if (this.websocket) this.websocket.close();
         this.websocket = new WebSocket((this.secure ? 'wss://' : 'ws://') + this.url, this.protocols);
-        this.websocket.addEventListener('error', this.error_handler);
-        this.websocket.addEventListener('close', this.close_handler);
+        this.websocket.addEventListener('error', this.error_handler.bind(this));
+        this.websocket.addEventListener('close', this.close_handler.bind(this));
         this.websocket.addEventListener('open', function(e) {
           this.retry_count = 0;
-          if (this.on_open) this.on_open(e);
+          clearTimeout(this.rety_id);
+          log('CONNECT', this.url);
+          if (this.on_open) this.on_open(this, e);
           if (this.on_data) {
             this.websocket.addEventListener('message', function(e) {
               var data = e.data;
               if (this.json) data = JSON.parse(data);
               log('MSG_IN', qp.stringify(data, true));
-              if (this.on_data) this.on_data(data, e);
+              if (this.on_data) this.on_data(data, this, e);
             }.bind(this));
           }
         }.bind(this));
       },
   
-      reconnect: function(e) {
+      reconnect: function() {
         if (this.retry_count++ < this.retry_max) {
-          setTimeout(function() {
-            if (this.on_reconnect) this.on_reconnect(e);
+          this.retry_id = setTimeout(function() {
+            log('RECONNECT', this.url);
+            if (this.on_reconnect) this.on_reconnect(this);
             this.connect();
           }.bind(this), this.retry_interval);
         } else if (this.on_reconnect_failed) {
@@ -3000,19 +3007,14 @@
       },
   
       error_handler: function(e) {
-        if (this.auto && e && e.code === 'ECONNREFUSED') {
-          this.reconnect();
-        } else if (this.on_error) {
-          this.on_error.call(this, e);
-        }
+        if (this.on_error) this.on_error(this, e);
+        this.close();
+        if (this.auto && e && (e.code === 'ECONNREFUSED' || e.code === 'ERR_CONNECTION_REFUSED')) this.reconnect();
       },
   
       close_handler: function(e) {
-        if (this.auto && e.code === 1005) {
-          this.reconnect();
-        } else if (this.on_close) {
-          this.on_close(this);
-        }
+        if (this.auto && e && (e.code === 1005 || e.code === 1006)) this.reconnect();
+        if (this.on_close) this.on_close(this, e);
       },
   
       send: function(data) {
