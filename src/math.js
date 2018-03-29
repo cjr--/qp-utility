4a85652bc091e440a9584af924a2db58930ec27f
cjr-- <cjr@hotmail.co.uk>
2018-03-29 21:04:22 +0100

    maths; random & easing fns

--- src/math.js
+++ src/math.js
@@ -31,6 +31,44 @@ function round(n, decimals) {
   return Number(Math.round(n + 'e' + decimals) + 'e-' + decimals);
 }
 
+function random(min, max) {
+  return Math.round(min + (Math.random() * (max -min)));
+}
+
+function random_pick(o) {
+  if (is_array(o)) {
+    return o[random(0, o.length - 1)];
+  }
+}
+
+function random_bool() {
+  return Math.random() > 0.5;
+}
+
+function in_range(n, min, max) {
+  return ((n >= min) && (n <= max));
+}
+
+function interpolate(a, b, percent) {
+  return a + (b - a) * (percent || 0.1);
+}
+
+function ease_in(a, b, percent) {
+  return a + (b - a) * Math.pow(percent, 2);
+}
+
+function ease_out(a, b, percent) {
+  return a + (b - a) * (1 - Math.pow(1 - percent, 2));
+}
+
+function ease_in_out(a, b, percent) {
+  return a + (b - a) * ((-Math.cos(percent * Math.PI) / 2) + 0.5);
+}
+
+function lerp(n, dn, dt) {
+  return n + (dn * dt);
+}
+
 function max_number(s, d) { return Number(repeat('9', s) + '.' + repeat('9', d)); }
 
 function truncate(n, decimals) {
