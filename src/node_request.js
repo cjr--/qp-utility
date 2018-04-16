9245795371eb5759888fb3f24475404ebbc2dd7d
cjr-- <cjr@hotmail.co.uk>
2018-04-16 19:45:42 +0100

    node_request; feature parity with browser_request

--- src/node_request.js
+++ src/node_request.js
@@ -1,4 +1,5 @@
 function http_request(options) {
+  options.url = url.parse(options.url);
   options.done = options.done || noop;
   if (options.bind) options.done.bind(options.bind);
   options.headers = options.headers || {};
@@ -8,6 +9,9 @@ function http_request(options) {
   var response = { ok: false };
   var request = {
     method: options.method || 'GET',
+    hostname: options.url.hostname,
+    port: options.url.port,
+    path: options.url.path,
     headers: options.headers || {},
     data: options.data || null
   };
@@ -15,45 +19,50 @@ function http_request(options) {
     request.method = 'POST';
     var json = JSON.stringify(options.json, null, '  ');
     if (json.length) request.data = json;
-    request.headers['Content-Type'] = 'application/json';
+    request.headers['Content-Type'] = 'application/json; charset=utf-8';
   } else if (options.text) {
     request.method = 'POST';
     request.data = options.text;
-    request.headers['Content-Type'] = 'text/plain';
+    request.headers['Content-Type'] = 'text/plain; charset=utf-8';
   } else if (options.html) {
     request.method = 'GET';
-    request.headers['Content-Type'] = 'text/html';
+    request.headers['Content-Type'] = 'text/html; charset=utf-8';
     request.data = options.html;
   } else {
     request.method = request.method.toUpperCase();
   }
+
+  request.headers['Content-Length'] = Buffer.byteLength(request.data || '');
+  request.headers['User-Agent'] = `nodejs/${process.version} (${process.arch} ${process.platform} v8/${process.versions.v8})`;
+
   var http_request = http.request(request, function(http_response) {
     http_response.setEncoding('utf8');
     response.headers = http_response.headers;
     response.status = http_response.statusCode;
     if (response.status >= 200 && response.status < 400) {
+      response.content_type = response.headers['content-type'];
+      response.json = /^application\/json/.test(response.content_type);
       response.ok = true;
-      response.text = null;
-      http_response.on('data', function(data) { response.text += data; });
-      http_response.on('end', function() {
-        if (response.headers['content-type'] === 'application/json') {
+      response.text = '';
+      http_response.on('data', (chunk) => response.text += chunk);
+      http_response.on('end', () => {
+        if (response.json) {
           try {
-            response.data = JSON.parse(response.text);
-          } catch (e) {
-            return options.done(e);
+            response.data = JSON.parse(trim(response.text));
+            response.result = response.data;
+          } catch (error) {
+            return options.done(error, response);
           }
         }
         options.done(null, response);
       });
     } else {
-      options.done({
-        status: response.status
-      }, null);
+      options.done({ status: response.status }, response);
     }
   });
-  http_request.on('error', options.done);
-  if (options.method === 'POST' && options.data) {
-    http_request.write(options.data);
+  http_request.on('error', (error) => options.done(error, {}));
+  if (options.method === 'POST' && request.data) {
+    http_request.write(request.data);
   }
   http_request.end();
 }
