9b4d1a0598a07d13031827097206dd1d8ea5f2ef
Chris Richardson <cjr@hotmail.co.uk>
2020-07-27 19:07:31 +0100

    add ordinal fn

--- src/date.js
+++ src/date.js
@@ -165,6 +165,12 @@ function date_time(dt) {
   };
 }
 
+function ordinal(n) {
+  var s = ['th', 'st', 'nd', 'rd'];
+  var v = n % 100;
+  return n + (s[(v - 20) % 10] || s[v] || s[0]);
+}
+
 function timer() {
   var start = new Date();
   var lap = start;
